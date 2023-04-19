784bb234592cf41db52ba8d0588168df76383925
chrisbanas <bananas595@gmail.com>
2023-04-18 17:37:41 -0700

    on frontend phase 2

--- authenticate-me/app/controllers/api/users_controller.rb
+++ authenticate-me/app/controllers/api/users_controller.rb
@@ -1,10 +1,27 @@
 class Api::UsersController < ApplicationController
+
+  wrap_parameters include: User.attribute_names + ['password']
+
+  # def show
+  #   render json: @user
+  # end
+
+
   def create
     @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      # render json: @user
+      render :show
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+
   end
-  
+
   private
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :username, :password)
   end
 end
